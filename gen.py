aba2421312f4454467fe8f7898598578e6c94142
“zhiwei.xu” <qwxuzhiwei@163.com>
2019-03-08 16:49:12 +0800

    init

---
+++ gen.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import mistune
+from string import Template
+
+md_list = []
+
+root = os.getcwd()  # 根目录
+dist = os.path.join(root, 'dist')
+
+template_html = """
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Hsu's Home</title>
+    <link href="https://cdn.bootcss.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./assets/style.css" />
+    <link rel="icon" href="./assets/favicon.ico">
+  </head>
+  <body>
+    <div class="container">
+      <div class="markdown-content">
+        ${content}
+      </div>
+    </div>
+  </body>
+</html>
+"""
+
+
+def create_env():
+    """创建 html 存放目录
+    1. 存在则删除
+    2. 创建
+    """
+    if os.path.isdir(dist):
+        print('[{}] exist, delete it'.format(dist))
+        shutil.rmtree(dist)
+    os.mkdir(dist)
+    os.mkdir(os.path.join(dist, 'assets'))
+
+
+def copy_files():
+    # index.html
+    shutil.copy2(os.path.join(root, 'index.html'), dist)
+    # avatar
+    shutil.copy2(os.path.join(root, 'assets/avatar.png'), dist + '/assets')
+    # favicon
+    shutil.copy2(os.path.join(root, 'assets/favicon.ico'), dist + '/assets')
+    # lessc style.less style.css
+    lessc = shutil.which('lessc')
+    cmd = '{0} {1} {2}'.format(
+        lessc,
+        os.path.join(root, 'assets/style.less'),
+        os.path.join(dist, 'assets/style.css')
+    )
+    print(cmd)
+    os.system(cmd)
+
+    for md in md_list:
+        with open(os.path.join(root, md), 'r') as fr:
+            content = fr.read()
+            with open(os.path.join(root, 'dist/{}.html'.format(os.path.splitext(md)[0])), 'w') as fw:
+                fw.write(Template(template_html).substitute(
+                    {"content": mistune.markdown(content)}
+                ))
+
+
+if __name__ == '__main__':
+    create_env()
+    copy_files()
---
+++ setup.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+# 安装 scl
+sudo yum install epel-release centos-release-scl 
+# 安装 Python 3.6
+sudo yum install rh-python36
+# 使用 Python 3.6 创建并安装项目环境
+scl enable rh-python36 - << \EOF
+npm install -g less
+pip3 install --upgrade pip
+pip3 install --user pipenv
+path="\$PATH:`python -m site --user-base`/bin"
+echo "export PATH=$path"  >> ~/.bashrc
+source ~/.bashrc
+pipenv install
+EOF
